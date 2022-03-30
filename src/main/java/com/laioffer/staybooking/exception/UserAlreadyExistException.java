21c53d6888fb2a8b9c62953f0bc3bbc495fddef5
“BruceZAI” <changjun.zai@outlook.com>
2022-03-29 21:04:07 -0700

    2022.3.29 update

---
+++ src/main/java/com/laioffer/staybooking/exception/UserAlreadyExistException.java
@@ -0,0 +1,10 @@
+package com.laioffer.staybooking.exception;
+
+public class UserAlreadyExistException extends RuntimeException {
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+}
+
+
+
